c0749e742877307a871a0df6f71df67fc57c1171
JoseTA <josetrenor@gmail.com>
2025-01-17 21:36:27 +0100

    c

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,12 +1,13 @@
 const usersApi = "https://67895c042c874e66b7d88056.mockapi.io/api/v1/users";
 export const fetchUsers = async () => {
-  try {
-    const response = await fetch(usersApi);
-    const data = await response.json();
-    const users = data[0].users;
-    console.log(users);
-    return users;
-  } catch (error) {
-    throw new Error("Algo ha salido mal");
-  }
+    try {
+        const response = await fetch(usersApi);
+        const data = await response.json();
+        const users = data[0].users;
+        console.log(users);
+        return users;
+    }
+    catch (error) {
+        throw new Error("Algo ha salido mal");
+    }
 };
---
+++ src/userRegister/userRegister.js
@@ -0,0 +1,3 @@
+import { fetchUsers } from "../api/usersApi";
+const x = fetchUsers();
+console.log(x);
