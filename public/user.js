0888a33406d9e5ebe92384b8efe6e052238f9cc8
JoseTA <josetrenor@gmail.com>
2025-01-17 20:06:17 +0100

    c

---
+++ public/user.js
@@ -0,0 +1,32 @@
+import {
+  displayUserName,
+  displayUserPhotoProfile,
+  displayUserNamePlants,
+  displayUserPlants,
+} from "../src/userProfile/ui";
+import { fetchUsers } from "../src/api/usersApi";
+import { fetchPlants } from "../src/api/plantsApi";
+const getUserFromURL = () => {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("username");
+};
+const loadUserData = async () => {
+  const userName = getUserFromURL();
+  if (userName) {
+    try {
+      const users = await fetchUsers();
+      const plants = await fetchPlants();
+      const user = users.find((u) => u.username === userName);
+      if (user) {
+        displayUserPhotoProfile(user);
+        displayUserName(user);
+        displayUserNamePlants(user.savedPlants);
+        /* displayPlantInformation(plants); */
+        displayUserPlants(plants, user.savedPlants);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+document.addEventListener("DOMContentLoaded", loadUserData);
