ca334d9d96ac41d391e85d95f7b073db8814d663
JoseTA <josetrenor@gmail.com>
2025-01-17 22:49:26 +0100

    c

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: ".", // Esto indica que la raíz del proyecto es el directorio actual
   build: {
+    outDir: "dist", // Los archivos construidos se colocarán en la carpeta 'dist'
     rollupOptions: {
       input: {
-        main: "./index.html", // Página principal
-        register: "./public/register.html", // Página de registro
-        login: "./public/login.html", // Página de login
+        main: "index.html", // Archivo principal en la raíz
+        register: "src/userRegister/register.html", // El archivo register estará en src/userRegister
       },
     },
-    outDir: "dist", // Genera la salida en 'dist'
   },
 });
