28b7f7ee204f36e0765cf11da78fe68173b7913b
JoseTA <josetrenor@gmail.com>
2025-01-17 19:57:20 +0100

    c

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist", // Utiliza 'dist' o cualquier otra carpeta para la salida de la compilación
+  },
+});
