cb2587e8046e3b31abb5b7b80bb08b8cc2152afe
JoseTA <josetrenor@gmail.com>
2025-01-17 19:36:30 +0100

    run build

---
+++ src/userProfile/ui.js
@@ -0,0 +1,47 @@
+export const displayUserPhotoProfile = (img) => {
+    const newImg = document.createElement("img");
+    const div = document.getElementById("user-container");
+    if (div && div instanceof HTMLDivElement) {
+        newImg.src = img.photo;
+        div.appendChild(newImg);
+    }
+};
+export const displayUserName = (name) => {
+    const newHeader = document.createElement("h2");
+    const div = document.getElementById("user-container");
+    if (div && div instanceof HTMLDivElement) {
+        newHeader.innerText = name.username;
+        div.appendChild(newHeader);
+    }
+};
+export const displayUserNamePlants = (plants) => {
+    const div = document.getElementById("user-container");
+    plants.forEach((plant) => {
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+        h3.innerText = plant.nickname;
+        p.innerText = plant.customNotes;
+        div?.appendChild(h3);
+        div?.appendChild(p);
+    });
+};
+//Esta función sería con la que se trabajaría la información de las plantas
+export const displayPlantInformation = (allPlants) => {
+    const div = document.getElementById("user-container");
+    const ul = document.createElement("ul");
+    allPlants.forEach((plants) => {
+        const li = document.createElement("li");
+        li.innerText = plants.description;
+        ul.appendChild(li);
+        div?.appendChild(ul);
+    });
+};
+export const displayUserPlants = (allPlants, userPlant) => {
+    userPlant.forEach((u) => {
+        allPlants.forEach((plant) => {
+            if (u.plantId === plant.id) {
+                console.log(u.nickname, ".", u.customNotes, plant.description);
+            }
+        });
+    });
+};
